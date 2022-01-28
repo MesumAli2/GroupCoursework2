8e500921a1ed34a3906c7bff814fbbc33c732519
MesumAli2 <93323387+MesumAli2@users.noreply.github.com>
2022-01-28 19:24:15 +0400

    Final Build
    Json data update

--- CWGroupCourseowork2/app.js
+++ CWGroupCourseowork2/app.js
@@ -1,9 +1,6 @@
 var express = require("express");
-// require your router (defined on the next page)
+// require your router 
 var apiRouter = require("./server");
 var app = express();
-//app.use(express.static(staticPath));
-// use your router
-// any URL that starts with '/api' will be sent to 'apiRouter', // such as '/api/users' and '/api/message'
 app.use( apiRouter);
-app.listen(5000);
+app.listen(3000);
--- CWGroupCourseowork2/server.js
+++ CWGroupCourseowork2/server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var api = express.Router(); 
 const courses = [
-    { 'topic': 'math', 'location': 'hendon', 'price': 100 },
-    { 'topic': 'math', 'location': 'colindale', 'price': 80 },
-    { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
-    { 'topic': 'math', 'location': 'golders green', 'price': 120 },
+    { 'topic': 'math', 'location': 'London', 'price': 100 },
+    { 'topic': 'math', 'location': 'Liverpool', 'price': 80 },
+    { 'topic': 'math', 'location': 'Oxford', 'price': 90 },
+    { 'topic': 'math', 'location': 'Bristol', 'price': 120 },
 ];
 const user = [{'email': 'user@email.com', 'password':'mypassword'}]
 
@@ -12,19 +12,17 @@ api.use(function(req, res, next) {
 next(); 
 
 })
+//Api endpoint lessons called by the client file index.html
 api.get("/lessons", function(req, res){
-  
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(JSON.stringify(courses));
 
 })
+//Api endpoint user
 api.get("/user", function(req, res){
-    
     res.end(JSON.stringify(user))
 
 })
-
-
 
 module.exports = api
